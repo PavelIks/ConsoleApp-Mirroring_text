dc3998d52ab80d5d0281a4a21becf9d5cd9fd0d0
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-08-28 12:47:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MirroringText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Тётя лёша не беременный!");
+
+            var str = "Тётя лёша не беременный!";
+            char[] s = str.ToCharArray();
+            Array.Reverse(s);
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
